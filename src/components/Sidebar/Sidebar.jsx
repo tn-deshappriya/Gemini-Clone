65a2cfe7fcd995faad05ea8f88d07338054f7199
Mr.No-Hat <thiniranilushandeshappriya@gmail.com>
2025-03-28 16:03:15 +0530

    4.Sidebar Top Side Created.

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,24 @@
 import React from "react";
-import "Sidebar.css";
+import "../Sidebar/Sidebar.css";
+import { assets } from "../../assets/assets";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <div className="top"></div>
+      <div className="top">
+        <img className="menu" src={assets.menu_icon} alt="menu-icon" />
+        <div className="new-chat">
+          <img src={assets.plus_icon} alt="plus_icon" />
+          <p>New Chat</p>
+        </div>
+        <div className="resent">
+          <p className="resent-title">Resent</p>
+          <div className="resent-entry">
+            <img src={assets.message_icon} alt="" />
+            <p>What is React ...</p>
+          </div>
+        </div>
+      </div>
       <div className="bottom"></div>
     </div>
   );
