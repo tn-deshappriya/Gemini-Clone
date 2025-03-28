8d4b250bcff468c12064b25c34c1d6d1edc470c8
Mr.No-Hat <thiniranilushandeshappriya@gmail.com>
2025-03-28 12:26:33 +0530

    2.Sidebar.jsx component setup structure.

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "Sidebar.css";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="top"></div>
+      <div className="bottom"></div>
+    </div>
+  );
+};
+export default Sidebar;
