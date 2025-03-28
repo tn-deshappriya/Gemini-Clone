46418cc4b05bc07a93af9b631031747988ec0e95
Mr.No-Hat <thiniranilushandeshappriya@gmail.com>
2025-03-28 16:30:26 +0530

    4.Sidebar Top And Bottom Section Created.

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,44 @@
 import React from "react";
+
 import "Sidebar.css";
+
+import "../Sidebar/Sidebar.css";
+import { assets } from "../../assets/assets";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="top"></div>
       <div className="bottom"></div>
+
+      <div className="top">
+        <img className="menu" src={assets.menu_icon} alt="menu-icon" />
+        <div className="new-chat">
+          <img src={assets.plus_icon} alt="plus_icon" />
+          <p>New Chat</p>
+        </div>
+        <div className="resent">
+          <p className="resent-title">Resent</p>
+          <div className="resent-entry">
+            <img src={assets.message_icon} alt="" />
+            <p>What is React ...</p>
+          </div>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="bottom-item  resent-entry">
+          <img src={assets.question_icon} alt="" />
+          <p>Help</p>
+        </div>
+        <div className="bottom-item  resent-entry">
+          <img src={assets.history_icon} alt="" />
+          <p>Activity</p>
+        </div>
+        <div className="bottom-item  resent-entry">
+          <img src={assets.setting_icon} alt="" />
+          <p>Settings</p>
+        </div>
+      </div>
     </div>
   );
 };
