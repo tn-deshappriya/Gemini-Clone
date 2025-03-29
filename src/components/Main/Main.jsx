9469aa97ffaf2503825ea11c99bf21ee8cfaa107
Mr.No-Hat <thiniranilushandeshappriya@gmail.com>
2025-03-29 23:27:42 +0530

    1.2.Bug Fix.

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../Main/Main.css";
+
+const Main = () => {
+  return (
+    <div className="main">
+      <div className="nav"></div>
+    </div>
+  );
+};
+export default Main;
