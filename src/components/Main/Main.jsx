10fec0d7f2651c3693be13d9134c9f390d152d9e
Mr.No-Hat <thiniranilushandeshappriya@gmail.com>
2025-04-04 11:31:50 +0530

    1.3.create main container and add Css cards,card and others.

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -9,6 +9,32 @@ const Main = () => {
         <p>Gemini</p>
         <img src={assets.user_icon} alt="" />
       </div>
+      <div className="main-container">
+        <div className="greet">
+          <p>
+            <span>Hello, Dev.</span>
+          </p>
+          <p>How can I help you today?</p>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <p>Suggest beautiful places to see on an upcoming road trip</p>
+            <img src={assets.compass_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Briefly summarize this concept: urban planning </p>
+            <img src={assets.bulb_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Brainstorm team bonding activities for our work retreat</p>
+            <img src={assets.message_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Improve the readbility of the following code</p>
+            <img src={assets.code_icon} alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
