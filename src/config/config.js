eb4895361ea4013b907d274e1657416eff67c958
Mr.No-Hat <thiniranilushandeshappriya@gmail.com>
2025-04-05 23:37:32 +0530

    1.7.Gemini API connection create.

---
+++ src/config/config.js
@@ -0,0 +1,13 @@
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({ apiKey: import.meta.env.VITE_GEMINI_API_KEY });
+
+async function runChat(prompt) {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash",
+    contents: prompt,
+  });
+  console.log(response.text);
+}
+
+export default runChat;
