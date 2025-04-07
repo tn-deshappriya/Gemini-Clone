496a20b1dd17cecb2991153df93059cbf121821a
Mr.No-Hat <thiniranilushandeshappriya@gmail.com>
2025-04-07 09:47:33 +0530

    2.2.Setup other functionality like proper loading , recents,newchats...

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -56,7 +56,15 @@ const Main = () => {
             </div>
             <div className="result-data">
               <img src={assets.gemini_icon} alt="" />
-              <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+              {loading ? (
+                <div className="loader">
+                  <hr />
+                  <hr />
+                  <hr />
+                </div>
+              ) : (
+                <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+              )}
             </div>
           </div>
         )}
